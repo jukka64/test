aa36b8134e388a551b5437cf30fd9fb7205e09a5
suuntain <suuntain@proton.me>
2025-10-07 13:19:46 +0300

    example project created by XCode 16

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Jukka Kemppainen on 7.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
